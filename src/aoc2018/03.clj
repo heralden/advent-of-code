cf016af99f766860a046bfed89ccac6b0371b97e
uosl <regen@anche.no>
2018-12-03 21:05:42 +0100

    Complete day 3

--- src/aoc2018/03.clj
+++ src/aoc2018/03.clj
@@ -1,4 +1,5 @@
-(ns aoc2018.03)
+(ns aoc2018.03
+  (:require [clojure.set :as s]))
 
 (defn read-claim [claim]
   (let [matcher (re-matcher #"\d+" claim)
@@ -21,6 +22,48 @@
 
 (defn place-claim [area {:keys [id x y w h]}]
   (let [indices (dims->indices area x y w h)]
-    (reduce #(assoc %1 %2 id) area indices)))
+    (reduce (fn [area index]
+              (update area
+                      index
+                      #(if (= % 0) id \X)))
+            area
+            indices)))
 
-(defn solve1 [claims])
+(defn greatest-xy [claims]
+  (reduce (fn [[gx gy] {:keys [x y w h]}]
+            [(max gx (+ x w))
+             (max gy (+ y h))])
+          [0 0]
+          claims))
+
+(defn init-area [claims]
+  (let [[gx gy] (greatest-xy claims)]
+    (with-meta (vec (repeat (* gx gy) 0))
+               {:width gx})))
+
+(defn solve1 [claim-strs]
+  (let [claims (map read-claim claim-strs)
+        area (init-area claims)]
+    (->> (reduce place-claim area claims)
+         (filter #(= % \X))
+         count)))
+
+(defn overlapping-claim-ids [[ids area] {:keys [id x y w h]}]
+  (let [indices (dims->indices area x y w h)]
+    (reduce (fn [[overlaps area] index]
+              (let [tile (get area index)]
+                (if (= tile 0)
+                  [overlaps (assoc area index id)]
+                  [(conj overlaps tile id)
+                   (assoc area index id)])))
+            [ids area]
+            indices)))
+
+(defn solve2 [claim-strs]
+  (let [claims (map read-claim claim-strs)
+        area (init-area claims)
+        all-ids (into #{} (map :id claims))
+        overlapping-ids (first (reduce overlapping-claim-ids
+                                       [#{} area]
+                                       claims)) ]
+    (first (s/difference all-ids overlapping-ids))))
--- test/aoc2018/03_test.clj
+++ test/aoc2018/03_test.clj
@@ -1,11 +1,18 @@
 (ns aoc2018.03-test
-  (:require [aoc2018.03 :refer [solve1]]
+  (:require [aoc2018.03 :refer [solve1 solve2]]
             [clojure.java.io :as io]
             [clojure.test :refer [deftest is]]))
 
-(def ids (->> "03" io/resource io/reader line-seq))
+(def claims (->> "03" io/resource io/reader line-seq))
+
+(def test-claims ["#1 @ 1,3: 4x4"
+                  "#2 @ 3,1: 4x4"
+                  "#3 @ 5,5: 2x2"])
 
 (deftest test1
-  (is (= 4 (solve1 ["#1 @ 1,3: 4x4"
-                    "#2 @ 3,1: 4x4"
-                    "#3 @ 5,5: 2x2"]))))
+  (is (= 4 (solve1 test-claims)))
+  (is (= 111266 (solve1 claims))))
+
+(deftest test2
+  (is (= 3 (solve2 test-claims)))
+  (is (= 266 (solve2 claims))))
